f2c35f8f13caa99fe74260bf9870d7a86e350700
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-19 08:57:32 -0300

    Estilização da página Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { View, Text, Image, ScrollView } from "react-native";
+import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
 
 export default function Start() {
   return (
-    <View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <Image source={require("../../assets/icons/logo3.png")} />
-          <Text>
+    <View style={styles.container}>
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../../assets/icons/logo3.png")}
+            style={styles.logo}
+          />
+          <Text style={styles.description}>
             Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"}{" "}
             o melhor nível.
           </Text>
@@ -16,3 +19,22 @@ export default function Start() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+});
