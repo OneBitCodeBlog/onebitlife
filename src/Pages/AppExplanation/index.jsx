3104fe4e29349a0d4e497831f77ca485b4a2530c
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-20 01:00:27 -0300

    Estrutura da página Home e Navegação

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  handleNavHome,
+  handleSetShowHome,
+} from "react-native";
 import DefaultButton from "../../Components/Common/DefaultButton";
 import ExplanationCard from "../../Components/Explanation/ExplanationCard";
+import { useNavigation } from "@react-navigation/native";
 
 export default function AppExplanation() {
-  function handleSetShowHome() {
-    console.log("Botão clicado!");
+  const navigation = useNavigation();
+
+  function handleNavHome() {
+    navigation.navigate("Home");
   }
 
   return (
@@ -25,7 +35,7 @@ export default function AppExplanation() {
           </Text>
           <DefaultButton
             buttonText={"Continuar"}
-            handlePress={handleSetShowHome}
+            handlePress={handleNavHome}
             width={250}
             height={50}
           />
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,55 @@
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+
+import LifeStatus from "../../Components/Common/LifeStatus";
+
+export default function Home() {
+  const navigation = useNavigation();
+
+  function handleNavExplanation() {
+    navigation.navigate("AppExplanation");
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
+
+          <LifeStatus />
+        </View>
+
+        <Text
+          style={styles.explanationText}
+          onPress={() => {
+            handleNavExplanation();
+          }}
+        >
+          Ver explicações novamente
+        </Text>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  dailyChecks: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    marginTop: 40,
+  },
+  explanationText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingTop: 15,
+    paddingBottom: 25,
+  },
+});
