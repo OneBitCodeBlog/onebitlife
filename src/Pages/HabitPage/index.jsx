2d51d7a46cad0f83f95dd5d5713a92fbcc1727d8
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-25 22:50:49 -0300

    Botão de Notificação (Toggle) e Texto

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, setHabitInput } from "react";
+import React, { useEffect, useState, useRef, setHabitInput } from "react";
 import {
   View,
   Text,
@@ -6,18 +6,19 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
-  Alert,
 } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
 import SelectHabit from "../../Components/HabitPage/SelectHabit";
 import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
+import Notification from "../../Components/HabitPage/Notification";
 
 export default function HabitPage({ route }) {
   const navigation = useNavigation();
   const [habitInput, setHabitInput] = useState();
   const [frequencyInput, setFrequencyInput] = useState();
+  const [notificationToggle, setNotificationToggle] = useState();
 
   const { create, habit } = route.params;
 
@@ -47,6 +48,12 @@ export default function HabitPage({ route }) {
               habitFrequency={habit?.habitFrequency}
               frequencyInput={setFrequencyInput}
             />
+            {frequencyInput === "Mensal" ? null : (
+              <Notification
+                notificationToggle={notificationToggle}
+                setNotificationToggle={setNotificationToggle}
+              />
+            )}
           </View>
         </View>
       </ScrollView>
