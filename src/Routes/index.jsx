c7c42c34dd9f8aa69f18176de8c5ce8e4bd481bb
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-27 01:51:52 -0300

    Renderização condicional de página e tempo de vida dinâmico

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
+import ChangeNavigationService from "../Services/ChangeNavigationService";
 import AllPages from "./AllPages";
 import HomePage from "./HomePage";
 
 export default function Routes() {
   const [showHome, setShowHome] = useState("false");
+  ChangeNavigationService.checkShowHome(1)
+    .then((showHome) => {
+      setShowHome(showHome.showHome);
+    })
+    .catch((err) => console.log(err));
 
   return <>{showHome === "true" ? <HomePage /> : <AllPages />}</>;
 }
