0201c08aae38ca95eb11469ba03f53de4e47f4b4
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-27 01:32:13 -0300

    Criando o serviço GET

--- src/Services/ChangeNavigationService.js
+++ src/Services/ChangeNavigationService.js
@@ -29,6 +29,23 @@ const setShowHome = (obj) => {
   });
 };
 
+const checkShowHome = (id) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM change_navigation where id=?;",
+        [id],
+        (_, { rows }) => {
+          if (rows.length > 0) resolve(rows._array[0]);
+          else reject("Obj not found: id=" + id);
+        },
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
+
 export default {
   setShowHome,
+  checkShowHome,
 };
