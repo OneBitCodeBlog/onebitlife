ce31ae378829373b27dbbe84cdf92466fcaf883d
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-27 01:11:39 -0300

    Criando serviço de SET

--- src/Services/ChangeNavigationService.js
+++ src/Services/ChangeNavigationService.js
@@ -9,3 +9,26 @@ db.transaction((tx) => {
     }
   );
 });
+
+const setShowHome = (obj) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO change_navigation (showHome, appStartData) values (?, ?);",
+        [obj.showHome, obj.appStartData],
+        (_, { rowsAffected, insertId }) => {
+          if (rowsAffected > 0) {
+            resolve(insertId);
+          }
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+};
+
+export default {
+  setShowHome,
+};
