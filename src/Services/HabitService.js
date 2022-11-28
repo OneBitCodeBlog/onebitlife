98cd35a9baa2a4fd35ebe5a8ea5fa5b730515417
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-27 22:06:03 -0300

    Criando o serviço de criação do hábito

--- src/Services/HabitService.js
+++ src/Services/HabitService.js
@@ -7,3 +7,34 @@ db.transaction((tx) => {
     }
   );
 });
+
+const createHabit = (obj) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO habits (habitArea, habitName, habitFrequency, habitHasNotification, habitNotificationFrequency, habitNotificationTime, lastCheck, daysWithoutChecks, progressBar, habitIsChecked, habitChecks) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+        [
+          obj.habitArea,
+          obj.habitName,
+          obj.habitFrequency,
+          obj.habitHasNotification,
+          obj.habitNotificationFrequency,
+          obj.habitNotificationTime,
+          obj.lastCheck,
+          obj.daysWithoutChecks,
+          obj.progressBar,
+          obj.habitIsChecked,
+          obj.habitChecks,
+        ],
+        (_, { rowsAffected, insertId }) => {
+          if (rowsAffected > 0) resolve(insertId);
+          else reject("Error inserting obj: " + JSON.stringify(obj));
+        },
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
+export default {
+  createHabit,
+};
