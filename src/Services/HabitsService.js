d6f84612e3c61b0e6d2338bb6bc1a8995c35308f
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-27 23:52:58 -0300

    Criando o serviço de Update Habit

--- src/Services/HabitsService.js
+++ src/Services/HabitsService.js
@@ -53,7 +53,31 @@ const findByArea = (habitArea) => {
   });
 };
 
+const updateHabit = (obj) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE habits SET habitName=?, habitFrequency=?, habitHasNotification=?, habitNotificationFrequency=?, habitNotificationTime=? WHERE habitArea=?;",
+        [
+          obj.habitName,
+          obj.habitFrequency,
+          obj.habitHasNotification,
+          obj.habitNotificationFrequency,
+          obj.habitNotificationTime,
+          obj.habitArea,
+        ],
+        (_, { rowsAffected }) => {
+          if (rowsAffected > 0) resolve(rowsAffected);
+          else reject("Error updating obj");
+        },
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
+
 export default {
   createHabit,
   findByArea,
+  updateHabit,
 };
