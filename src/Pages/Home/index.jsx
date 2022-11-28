c76d142f1f36b385014089f6f2191f06b879fa1f
julianaconde <alvarez.juliana.c@gmail.com>
2022-11-28 00:38:32 -0300

    Linkando a página com o serviço Delete Habit

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -24,6 +24,8 @@ export default function Home({ route }) {
     navigation.navigate("AppExplanation");
   }
 
+  const excludeArea = route.param?.excludeArea;
+
   useEffect(() => {
     HabitsService.findByArea("Mente").then((mind) => {
       setMindHabit(mind[0]);
@@ -37,6 +39,21 @@ export default function Home({ route }) {
     HabitsService.findByArea("Humor").then((fun) => {
       setFunHabit(fun[0]);
     });
+
+    if (excludeArea) {
+      if (excludeArea == "Mente") {
+        setMindHabit(null);
+      }
+      if (excludeArea == "Financeiro") {
+        setMoneyHabit(null);
+      }
+      if (excludeArea == "Corpo") {
+        setBodyHabit(null);
+      }
+      if (excludeArea == "Humor") {
+        setFunHabit(null);
+      }
+    }
 
     ChangeNavigationService.checkShowHome(1)
       .then((showHome) => {
